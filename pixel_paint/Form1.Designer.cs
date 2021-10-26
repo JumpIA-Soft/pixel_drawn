1da573782b83391049f3c32946e2a47400e402d0
alex-vinicio <alexandino2011@gmail.com>
2021-10-26 05:44:25 -0500

    add pixel drawing

---
+++ pixel_paint/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace pixel_paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crearPixel = new System.Windows.Forms.Button();
+            this.marco = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.marco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // crearPixel
+            // 
+            this.crearPixel.Location = new System.Drawing.Point(55, 129);
+            this.crearPixel.Name = "crearPixel";
+            this.crearPixel.Size = new System.Drawing.Size(146, 32);
+            this.crearPixel.TabIndex = 0;
+            this.crearPixel.Text = "Crear pixel random";
+            this.crearPixel.UseVisualStyleBackColor = true;
+            this.crearPixel.Click += new System.EventHandler(this.crearPixel_Click);
+            // 
+            // marco
+            // 
+            this.marco.Location = new System.Drawing.Point(243, 46);
+            this.marco.Name = "marco";
+            this.marco.Size = new System.Drawing.Size(400, 300);
+            this.marco.TabIndex = 1;
+            this.marco.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.marco);
+            this.Controls.Add(this.crearPixel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.marco)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button crearPixel;
+        private System.Windows.Forms.PictureBox marco;
+    }
+}
+
---
+++ pixel_paint/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pixel_paint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pixelVector = new Bitmap(400, 300); // crear un objeto tipo Bitmap=crear pixel o pixel
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crearPixel_Click(object sender, EventArgs e)
+        {
+            Color color0; //definimos un atributo de tipo Color
+            
+            //color0 = Color.BlueViolet; // asignamos el color Rojo
+
+            color0 = Color.FromArgb(0, 0, 0); //asignamos un color segun un forma to RBG https://docs.microsoft.com/en-us/dotnet/api/system.drawing.color.toargb?view=net-5.0#System_Drawing_Color_ToArgb
+
+            int X = 1, Y = 2;
+            for (int i = 0; i < 100; i++)
+            {
+                X = i;
+                if (X % 2 != 0)
+                {
+                    pixelVector.SetPixel(X, Y, color0);//funcion por defecto para asignar una poscion en la pantalla con el color
+                }
+            }
+
+            marco.Image = pixelVector; //asignamos los valores del pixel al objeto imagen de la GUI
+        }
+    }
+}
